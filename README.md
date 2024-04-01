37de8f2fce5f476d1a51edc1f213a3291cf0087f
nghiattr <nghiavodich2@gmail.com>
2024-04-02 03:43:21 +0700

    Final

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-[![Build Status](https://dev.azure.com/alemag1986/ensuring-quality-releases/_apis/build/status/ensuring-quality-releases?branchName=master)](https://dev.azure.com/alemag1986/ensuring-quality-releases/_build/latest?definitionId=5&branchName=master)
-
-[[_TOC_]]
 
 # Overview
 
@@ -10,7 +7,7 @@ Udacity final project Ensuring Quality Releases.
 
 # Project Structure 
 
-- **.devops/pipelines**: azure pipelines yaml
+- **azure-pipelines.yaml**: azure pipelines yaml
 - **automatedtesting**: suites of different tests
   - **jmeter**: load test (JMeterPlan.jmx), CSV inputs, and TestReports (endurance-report, stress-report)
   - **postman**: functional tests postman collections and environments
@@ -27,44 +24,50 @@ Udacity final project Ensuring Quality Releases.
 
 - Terraform to apply Infrastructure as Code (IaC)
   - screenshot of the log output of Terraform when executed by the CI/CD pipeline
-   ![capture-1-terraform-output](screenshots/capture-1-terraform-output.png)
-   ![capture-1-terraform-state](screenshots/capture-1-terraform-state.png)
+   ![capture-terraform-output](screenshots/1.png)
+   ![capture-terraform-state](screenshots/2.png)
 
 - Automated testing tasks
   - screenshot of the successful execution of the pipeline build results page (/_build/results?buildId={id}&view=results)
-    ![capture-2-pipeline](screenshots/capture-2-pipeline.png)
+    ![capture-pipeline](screenshots/3.png)
+
+## Webapp Depdeloyment
+
+- Deploy Azure Webapp http://was-eqr.azurewebsites.net/
+  ![](screenshots/4.png)
 
 ## Automated Testing
 
 - Load test suite 
   - screenshot of the log output of JMeter when executed by the CI/CD pipeline
-    ![capture-3-load-test](screenshots/capture-3-load-test-part1.png)
-    ![capture-3-load-test](screenshots/capture-3-load-test-part2.png)
+    ![capture-load-test](screenshots/5.png)
+    ![capture-load-test](screenshots/6.png)
   
 - Functional test suites 
   - screenshot of the execution of the test suite by the CI/CD pipeline
-   ![capture-4-functional-test](screenshots/capture-4-functional-test.png)
+   ![capture-functional-test](screenshots/7.png)
 
 - API-integration tests
   - screenshot of the Run Summary page (which contains 4 graphs)
-    ![capture-5-summary-page-part1](screenshots/capture-5-summary-page-part1.png)
+    ![capture-summary-page-part1](screenshots/8.png)
+    ![capture-summary-page-part1](screenshots/9.png)
   - screenshot of the Test Results page (which contains the test case titles from each test) 
-    ![capture-5-test-result-page-part2](screenshots/capture-5-test-result-page-part2.png)
+    ![capture-test-result-page-part2](screenshots/10.png)
   - screenshot of the output of the Publish Test Results step
-    ![capture-5-publish-tests-output-part3](screenshots/capture-5-publish-tests-output-part3.png)
+    ![capture-publish-tests-output-part3](screenshots/11.png)
 
 ## Monitoring & Observability
 
 - Configure Azure Monitor
   - screenshots of the email received when the alert is triggered
-    ![capture-6-emails-part1](screenshots/capture-6-emails-part1.png)
+    ![capture-emails-part1](screenshots/12.png)
   - screenshots of the graphs of the resource that the alert was triggered
-    ![capture-6-webapp-part2](screenshots/capture-6-webapp-part2.png)
+    
+    ![capture-alerts-part3a](screenshots/14.png)
   - screenshots of the alert rule
-    ![capture-6-alerts-part3a](screenshots/capture-6-alerts-part3c.png)
-    ![capture-6-alerts-part3b](screenshots/capture-6-alerts-part3c.png)
-    ![capture-6-alerts-part3c](screenshots/capture-6-alerts-part3c.png)
+    ![capture-webapp-part2](screenshots/13.png)
+
 
 - Azure Log Analytics
   - screenshots of log analytics queries and result sets which will show specific output of the Azure resource
-    ![capture-7-log-analytics](screenshots/capture-7-log-analytics.png)
+    ![capture-log-analytics](screenshots/15.png)
--- automatedtesting/selenium/uitests.py
+++ automatedtesting/selenium/uitests.py
@@ -3,6 +3,9 @@ import time
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #datetime.now().strftime("%m-%d-%y %H:%M:%S")
 
@@ -11,75 +14,60 @@ inventory_url = 'https://www.saucedemo.com/inventory.html'
 cart_url = 'https://www.saucedemo.com/cart.html'
 
 def create_driver():
-    print ('Starting the browser...')
+    print('Starting the browser...')
     options = ChromeOptions()
     options.add_argument("--headless") 
     return webdriver.Chrome(options=options)
     
 
 # Start the browser and login with standard_user
-def test_login (driver, user, password):
-    print ('Test: login. Navigating to the demo page to login {}'.format(login_url))
+def test_login(driver, user, password):
+    print('Test: login. Navigating to the demo page to login {}'.format(login_url))
     driver.get(login_url)
-    print ('Login attempt, user: {},  password: {}'.format(user, password))
-    driver.find_element_by_id('user-name').send_keys(user)
-    driver.find_element_by_id('password').send_keys(password)
-    driver.find_element_by_id('login-button').click()
-    #print ('Assert in inventory page. ')
+    print('Login attempt, user: {},  password: {}'.format(user, password))
+    driver.find_element(By.ID, 'user-name').send_keys(user)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID, 'login-button').click()
     assert inventory_url in driver.current_url
-    #print ('User successfully logged in.')
-    print ('Test Login Success.')
+    print('Test Login Success.')
+
     
 
 def test_add_items_to_cart(driver):
     items_in_cart = []
-    print ('Test: adding items to cart')
-    elements = driver.find_elements_by_class_name('inventory_item')
-    #print('Total items to add: {}'.format(len(elements)))
+    print('Test: adding items to cart')
+    elements = driver.find_elements(By.CLASS_NAME, 'inventory_item')
     for item in elements:
-        item_name = item.find_element_by_class_name('inventory_item_name').text
+        item_name = item.find_element(By.CLASS_NAME, 'inventory_item_name').text
         items_in_cart.append(item_name)
-        item.find_element_by_class_name('btn_inventory').click()
+        item.find_element(By.CLASS_NAME, 'btn_inventory').click()
         print('Added {} to cart'.format(item_name))
-    #print ('Assert in cart icon to reflect {} items added.'.format(len(elements)))
-    cart_element = driver.find_element_by_class_name('shopping_cart_badge')
+    cart_element = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
     assert int(cart_element.text) == len(elements)
-    #print ('Navigate to cart and assert items in cart.')
-    driver.find_element_by_class_name('shopping_cart_link').click()
-    #print ('Assert in cart page. ')
+    driver.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
     assert cart_url in driver.current_url
-    #print ('Assert items in cart. ')
-    for item in driver.find_elements_by_class_name('inventory_item_name'):
+    for item in driver.find_elements(By.CLASS_NAME, 'inventory_item_name'):
         assert item.text in items_in_cart
-    #print ('Asserted items in cart ') 
-    print ('Test Add Items in cart Success.')
+    print('Test Add Items in cart Success.')
 
 
 def test_remove_items_from_cart(driver):
-    print ('Test: removing items from cart')
-    #print ('Navigate to cart and assert items in cart.')
-    driver.find_element_by_class_name('shopping_cart_link').click()
-    #print ('Assert in cart page.')
+    print('Test: removing items from cart')
+    driver.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
     assert cart_url in driver.current_url
-
-    print("Items in Cart: {}".format(len(driver.find_elements_by_class_name('cart_item'))))
+    print("Items in Cart: {}".format(len(driver.find_elements(By.CLASS_NAME, 'cart_item'))))
     
-    #print('Remove all items from cart.')
-    for item in driver.find_elements_by_class_name('cart_item'):
-        item_name = item.find_element_by_class_name('inventory_item_name').text
-        item.find_element_by_class_name('cart_button').click()
+    for item in driver.find_elements(By.CLASS_NAME, 'cart_item'):
+        item_name = item.find_element(By.CLASS_NAME, 'inventory_item_name').text
+        item.find_element(By.CLASS_NAME, 'cart_button').click()
         print('Removed {} from cart'.format(item_name))
 
-    #print('Assert cart is empy')
-    #print("Items in Cart: {}".format(len(driver.find_elements_by_class_name('cart_item'))))
-    assert len(driver.find_elements_by_class_name('cart_item')) == 0
-    #print('Cart empty.')
-    print ('Test Remove Items from cart Success.')
+    assert len(driver.find_elements(By.CLASS_NAME, 'cart_item')) == 0
+    print('Test Remove Items from cart Success.')
 
 
 def run_ui_tests():
     driver = create_driver()
-    #print("Browser started successfully.")
     print("UI Tests started")
     
     print("####################################### - test login user")
